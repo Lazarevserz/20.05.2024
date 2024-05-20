f977b254ec519bdbab1df59c3974ccd94224a797
lazarevserz17@gmail.com <lazarevserz17@gmail.com>
2024-05-20 23:25:20 +0300

    новая ветка

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._05._2024
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("Команда разработчиков!");
+        }
+    }
+}
